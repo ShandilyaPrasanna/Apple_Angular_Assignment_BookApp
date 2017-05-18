994f5181150b5184cf2888d552e598891935c6eb
prasanna shandilya <prasanna shandilya>
2017-05-19 01:53:38 +0530

    Angular Book App using Json text file as a db

--- controller.js
+++ controller.js
@@ -10,20 +10,71 @@ console.log(error);
 });
 
 $scope.submitReview=function(index,id,star,rev,email){
-var obj={'star':"5",'comment':rev,'email':email};
+if(validator(id,email)){
+	console.log(id);
+var obj={'star':star,'comment':rev,'email':email};
 $scope.books[index].reviews.push(obj);
-console.log($scope.books);
-console.log("id",id);
-
 var review=$scope.books[index];
-var data=JSON.stringify(review);
-console.log(data);
-url="http://localhost:3000/books/"+id;
+var data=angular.toJson(review);
+var ad=index+1;
+url="http://localhost:3000/books/"+ad;
 console.log(url);
-$http.patch(url,data).then(function (responce){
-console.log(responce);
+$http.patch(url,data).then(function (responce){	
 },function (error){
 console.log(error);
 });
-}});
+}
+else{
+	alert("Enter Valid Data");
+}
+}
+
+$scope.cart=function(){
+	alert("Added to cart successfully");
+}
+
+$scope.validate=function(c){
+	if(c==1)
+		var i="star";
+	if(c==2)
+		var i="comment";
+	if(c==3)
+		var i="email";
+	
+	var inputVal = document.getElementById(i);
+    if (inputVal.value == "") {
+        inputVal.style.borderColor = "red";
+    }
+else
+{   
+	inputVal.style.borderColor = "green";
+}
+if(c==3){
+	var Val = document.getElementById(i).value;
+	if(!validateEmail(Val)){
+      inputVal.style.borderColor = "red";
+	}
+	else{
+		 inputVal.style.borderColor = "green";
+	}
+}
+}
+
+function validator(i,email){
+if(i.star.$valid && i.comment.$valid && validateEmail(email)){
+return true;
+}
+return false;
+};
+
+});
+
+function validateEmail(x) {
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        return false;
+    }
+    return true;
+}
 
